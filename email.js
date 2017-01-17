000b3484198cbd82c62270ff50734ba9f9f6749e
Marco Roganovic <marcoroganovic@gmail.com>
2017-01-17 21:56:27 +0100

    add regex for email validtion

---
+++ email.js
@@ -0,0 +1,9 @@
+function isEmail(input) {
+  return /^\w+[._]?\w+?@\w+[_-]*?\w+?\.\w{2,}/.test(input);
+}
+
+console.log(isEmail("marcoroganovic@gmail.com"));
+console.log(isEmail("123email123@gmail.com"));
+console.log(isEmail("test_test@email-server.net"))
+console.log(isEmail("hello@"));
+console.log(isEmail("@emailserver.net"))
